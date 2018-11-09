6567c21c12160e2dd6056fc420d40b4f07b72ad0
MimoN <mimo.ouyang@daocloud.io>
2018-11-09 17:24:59 +0800

    fix add the error redirection

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 import {
   HashRouter as Router,
   Route,
+  Redirect,
   Switch,
 } from 'react-router-dom';
 import Index from './view/pages/index';
@@ -14,6 +15,9 @@ ReactDOM.render(
     <Switch>
       <Route exact path='/' component={Index} />
       <Route path='/test' name='test' component={() => <div>This is the test page</div>} />
+      <Route path="*">
+        <Redirect to="/"></Redirect>
+      </Route>
     </Switch>
   </Router>,
   document.getElementById('root') as HTMLElement
