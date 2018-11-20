cd93a5c528acccd008628c729e2f90b4f24becb8
MimoN <mimo.ouyang@daocloud.io>
2018-11-20 17:13:30 +0800

    add the index page test case

--- src/view/pages/index/index.test.tsx
+++ src/view/pages/index/index.test.tsx
@@ -1,22 +1,49 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { createHashHistory } from 'history';
 import Index from '.';
 
+import { electron, fs } from '../../../../test/mocks';
+
 describe('<Index />', () => {
+  window.electron = electron;
+  window.fs = fs;
   it('renders without crashing', () => {
     shallow(<Index />);
   });
 
-  // it('switch to other page', () => {
-  //   window.electron = electron
-  //   const history = createHashHistory()
-  //   const index = new ReactWrapper(<Index history={history} />);
-  //   const startBtn = index.find('button.start-btn');
-  //   const settingsBtn = index.find('div.settings-btn');
-  //   startBtn.simulate('click');
-  //   expect(history.location.pathname).toEqual('/check');
-  //   settingsBtn.simulate('click');
-  //   expect(history.location.pathname).toEqual('/settings');
-  // })
+  it('switch to settings page', () => {
+    const history = createHashHistory()
+    const index = mount(<Index history={history} />);
+    const settingsBtn = index.find('div.settings-btn');
+    settingsBtn.simulate('click');
+    expect(history.location.pathname).toEqual('/settings');
+  })
+
+  it('settings complete', () => {
+    const history = createHashHistory()
+    const index = mount(<Index history={history} />);
+    const startBtn = index.find('button.start-btn');
+    startBtn.simulate('click');
+    setTimeout(() => {
+      expect(history.location.pathname).toEqual('/check');
+    })
+  })
+
+  it('settings empty', () => {
+    const history = createHashHistory()
+    const index = mount(<Index history={history} />);
+    const startBtn = index.find('button.start-btn');
+    startBtn.simulate('click');
+    expect(history.location.pathname).toEqual('/settings');
+  })
+
+  it('settings error', () => {
+    const history = createHashHistory()
+    const index = mount(<Index history={history} />);
+    const startBtn = index.find('button.start-btn');
+    startBtn.simulate('click');
+    expect(history.location.pathname).toEqual('/settings');
+  })
+  
 })
